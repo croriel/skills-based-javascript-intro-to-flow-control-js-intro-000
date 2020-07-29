8886023853de5dfdcf5fd71c998428ec95907ace
Caitlin Oriel <croriel@gmail.com>
2020-07-29 18:30:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,25 +28,13 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
   return age >= 13 && <=19 ? "You are a teenager" : "You are not a teenager"
 }
-function switchAge(age) {
-  switch (age) {
-    case 13:
-      return "You're a teenager";
-    case 14:
-      return "You're a teenager";
-    case 15:
-      return "You're a teenager";
-    case 16:
-      return "You're a teenager";
-    case 17:
-      return "You're a teenager";
-    case 18:
-      return "You're a teenager";
-    case 19:
-      return "You're a teenager";
-break;
-    default:
-      return "You have an age";
 
+function switchAge(age) {
+
+  switch(age <= 19 && age >= 13) {
+    case true :
+    return "You are a teenager"
+    default:
+    return "You have an age"
   }
 }
